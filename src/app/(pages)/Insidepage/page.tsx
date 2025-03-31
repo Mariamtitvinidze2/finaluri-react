0ae989d1496620634e3bc5df0fe60540f88d81b2
mariam titvinidze <titvinidzemariam3@gmail.com>
2025-03-31 11:50:21 +0400

    8

--- src/app/(pages)/Insidepage/page.tsx
+++ src/app/(pages)/Insidepage/page.tsx
@@ -1,6 +1,7 @@
 
-import Header from '@/app/components/__molecules/Header'
+import Header from '../../../app/components/__molecules/Header'
 import React from 'react'
+
 
 const page = () => {
   return (
